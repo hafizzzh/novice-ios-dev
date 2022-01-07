db80feb48ff4d2838aa0c749f5c7c8df83b4f5f5
hafizzzh <ibrahimbaim13@gmail.com>
2022-01-07 21:00:05 +0700

    Initial Commit

---
+++ habisinmobile/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  habisinmobile
+//
+//  Created by Ibrahim Hafizhan Witsqa on 07/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ habisinmobile/habisinmobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  habisinmobileApp.swift
+//  habisinmobile
+//
+//  Created by Ibrahim Hafizhan Witsqa on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct habisinmobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
